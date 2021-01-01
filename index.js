92958f011a8b8b8a3e7924165a817ba5ce49f897
Svetloslav15 <svetli.novoselski@gmail.com>
2021-01-01 23:40:18 +0200

    initial create

---
+++ index.js
@@ -0,0 +1,32 @@
+let clipPath = '';
+function generateWave() {
+    const width_px = +document.getElementById('width').value;
+    const height_px = +document.getElementById('height').value;
+    const offset = +document.getElementById('offset').value;
+    const amplitude = +document.getElementById('amplitude').value;
+    const frequency = +document.getElementById('frequency').value;
+    const units = frequency * width_px / 100;
+
+    let clipPathString = 'clip-path: polygon(100% 0%, 0% 0% ';
+
+    for (let i = 0; i <= 100; i++) {
+        let val = offset + amplitude * Math.cos(i / units);
+        val = (val / height_px * 100).toFixed(2);
+        clipPathString += ', ' + i + '% ' + val + '%';
+    }
+    clipPathString += ');';
+
+    clipPath = clipPathString;
+    let divEl = document.getElementById('wave-container');
+    divEl.style = `width:${width_px}px;height:${height_px}px; ` + 'background-color:#ED4545; transform: rotate(180deg); ' + clipPathString;
+}
+
+function getClipPath() {
+    let divEl = document.getElementById('result');
+    divEl.style = 'display: block';
+    divEl.textContent = clipPath;
+}
+
+(() => {
+    generateWave();
+})();
