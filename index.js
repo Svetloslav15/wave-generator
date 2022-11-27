0e3334af89d633ab7778f3524fa6836dd30c1048
Svetloslav Novoselski <svetli.novoselski@gmail.com>
2022-11-27 10:16:38 +0200

    add improvements from sanbec

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 let clipPath = '';
 function generateWave() {
-    const width_px = +document.getElementById('width').value;
-    const height_px = +document.getElementById('height').value;
-    const offset = +document.getElementById('offset').value;
-    const amplitude = +document.getElementById('amplitude').value;
-    const frequency = +document.getElementById('frequency').value;
-    const units = frequency * width_px / 100;
+    const width_px = +document.querySelector('#width').value;
+    const height_px = +document.querySelector('#height').value;
+    const offset = +document.querySelector('#offset').value;
+    const amplitude = +document.querySelector('#amplitude').value;
+    const frequency = +document.querySelector('#frequency').value;
+    const phase = +document.querySelector('#phase').value;
+    const points = +document.querySelector('#points').value;
+    const inverted = document.querySelector('#inverted').checked;
+    const units = 2 * Math.PI * frequency / points;
+    const path = 'clip-path: polygon(100% 100%, 0% 100% ';
+    const invertedpath = 'clip-path: polygon(100% 0%, 0% 0% ';
 
-    let clipPathString = 'clip-path: polygon(100% 0%, 0% 0% ';
+    let clipPathString = path;
+    if (inverted) clipPathString = invertedpath;
 
-    for (let i = 0; i <= 100; i++) {
-        let val = offset + amplitude * Math.cos(i / units);
-        val = (val / height_px * 100).toFixed(2);
-        clipPathString += ', ' + i + '% ' + val + '%';
+    let radPhase = phase * Math.PI / 180;
+
+    for (let i = 0; i <= points; i++) {
+        let val = offset + amplitude * Math.cos(i * units + radPhase);
+        let valY = (val / height_px * 100).toFixed(2);
+        let valX = (i * 100 / points).toFixed(2);
+        clipPathString += ', ' + valX + '% ' + valY + '%';
     }
     clipPathString += ');';
 
     clipPath = clipPathString;
     let divEl = document.getElementById('wave-container');
-    divEl.style = `width:${width_px}px;height:${height_px}px; ` + 'background-color:#ED4545; transform: rotate(180deg); ' + clipPathString;
+    divEl.style = `width:${width_px}px;height:${height_px}px; ` + clipPathString;
     getClipPath();
 }
 
@@ -29,7 +38,7 @@ function getClipPath() {
 
     const copyButton = document.getElementById('copyButton');
     copyButton.style = 'display: block;';
-    let container = document.getElementById('container');
+    let controls = document.getElementById('controls');
 }
 
 function copyToClipBoard() {
